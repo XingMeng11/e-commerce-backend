4e7e3f8f261ad3e9af3da8a8ca1125468334fad4
Xing Meng <mengxingmimi@gmail.com>
2022-06-07 17:13:41 -0700

    added routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+const db = require('./config/connection.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
+db.sync()
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
